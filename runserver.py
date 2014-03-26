5c868660e418ec0a5aee579d9195f1b88081ebee
Benjamin Attal <attal.benjamin@gmail.com>
2014-03-25 22:50:06 -0400

    Run tasks while starting up

--- runserver.py
+++ runserver.py
@@ -7,7 +7,11 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         app.debug = True
+        clear()
+        load()
+        load_images()
         app.run(host='0.0.0.0', port=5000)
+
     elif len(sys.argv) == 2:
         if sys.argv[1] == 'clear':
             clear()
